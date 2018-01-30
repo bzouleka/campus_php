9fa91e2ca737ae481db022d85b754e5696ffb212
bzouleka <bzouleka@gmail.com>
2018-01-30 15:16:53 +0100

    extraire les email

---
+++ exercice2.php
@@ -0,0 +1,22 @@
+<?php
+
+$users =  json_decode(file_get_contents('data.json'));
+
+
+function getEmail($email)
+{
+    echo $email -> email;
+}
+
+foreach ($users as $user) {
+    echo getEmail($user);
+}
+
+
+
+/*
+Créer une fonction getEmail(). Cette fonction qui prend en parametre une ligne du tableau $users
+doit retourner l'adresse email.
+
+Résultat attendu: Pour chaque ligne du tableau $users la fonction appelée doit retourner l'adresse email.
+*/
